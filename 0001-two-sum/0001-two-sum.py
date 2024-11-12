dc756ce5649fe38ae28af6ccfa13e0f80046c210
Woodstock <125184576+42jasuh@users.noreply.github.com>
2024-11-12 16:37:33 +0900

    Time: 0 ms (100%), Space: 17.9 MB (12.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Dictionary to store the numbers we've seen so far and their indices
-        num_to_index = {}
+        dic = {}
+        answer = []
+        for i in range(len(nums)):
+            pair = target - nums[i]
+            if pair not in dic:
+                dic[nums[i]] = i
+            elif pair in dic:
+                answer = [i, dic[pair]]
+        return sorted(answer)
+
+            
         
-        # Iterate through the array
-        for index, num in enumerate(nums):
-            # Calculate the complement
-            complement = target - num
-            
-            # Check if the complement is in the dictionary
-            if complement in num_to_index:
-                # If found, return the indices of the current number and the complement
-                return [num_to_index[complement], index]
-            
-            # If not found, add the current number and its index to the dictionary
-            num_to_index[num] = index        
